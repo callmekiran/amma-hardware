425c069541bc101cf00676ec06acd4649cdb725a
callmekiran <kiransooryakiran@gmail.com>
2026-02-03 16:51:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function openWhatsApp() {
+  window.open("https://wa.me/919995040555?text=Hello%20I%20want%20to%20order");
+}
+function openWhatsApp() {
+  window.open("https://wa.me/919995040555?text=Hello Amma Hardware, I want to enquire about your products");
+}
